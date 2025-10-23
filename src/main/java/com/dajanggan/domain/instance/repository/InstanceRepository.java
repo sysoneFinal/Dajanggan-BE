f84ab88855f69d3c38b248445e230e908482e996
mimmimkim <mynseoo@gmail.com>
2025-10-23 14:47:53 +0900

    feat: 인스턴스 등록 백 구현

--- src/main/java/com/dajanggan/domain/instance/repository/InstanceRepository.java
+++ src/main/java/com/dajanggan/domain/instance/repository/InstanceRepository.java
@@ -1,15 +1,11 @@
 package com.dajanggan.domain.instance.repository;
 
 import com.dajanggan.domain.instance.domain.Instance;
-import com.dajanggan.domain.instance.dto.InstanceDto;
 import org.apache.ibatis.annotations.Mapper;
-
-import java.util.List;
-import java.util.Optional;
 
 @Mapper
 public interface InstanceRepository {
-    int insertInstance(Instance instance);   // ✅ Entity로 통일 + insert 결과 row수 반환
+    void insertInstance(Instance instance);
 
 }
 
