0ba8a3fdde9f178ff11b0d5d535dd1bf253b0752
dongcarry96 <fhakwpak6@naver.com>
2025-11-08 22:24:41 +0900

    refactor: CrossOrigin 제거

--- src/main/java/com/dajanggan/domain/engine/checkpoint/controller/CheckpointController.java
+++ src/main/java/com/dajanggan/domain/engine/checkpoint/controller/CheckpointController.java
@@ -5,7 +5,6 @@ import com.dajanggan.domain.engine.checkpoint.service.CheckpointService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
