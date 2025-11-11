79dc91cf7094aeafd0f5438495d9b7814d20c9dd
mimmimkim <92439788+mimmimkim@users.noreply.github.com>
2025-11-11 09:14:19 +0900

    Feature/dj 259 vacuum detail (#54)
    * fix: eat: 상세 페이지 구현
    
    * fix: vacuum maintenance progress 수정

--- src/main/java/com/dajanggan/domain/session/dto/SessionDetailDto.java
+++ src/main/java/com/dajanggan/domain/session/dto/SessionDetailDto.java
@@ -1,12 +1,11 @@
 package com.dajanggan.domain.session.dto;
 
-/*import lombok.*;
+import lombok.*;
 
 @Setter
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
-@Getter*/
+@Getter
 public class SessionDetailDto {
 
 }
